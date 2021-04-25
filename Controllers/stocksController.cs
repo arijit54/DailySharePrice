f3e16aa0b2da2084966fdb1ec0036d55c6eaa025
arijit54 <duttaarijit54@gmail.com>
2021-04-25 16:32:45 +0530

    initialCommit

---
+++ Controllers/stocksController.cs
@@ -0,0 +1,36 @@
+﻿using DailySharePrice.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailySharePrice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class stocksController : ControllerBase
+    {
+        private IInMemoryStockDb stockDbObj;
+        public stocksController(IInMemoryStockDb obj)
+        {
+            stockDbObj = obj;
+        }
+        [HttpGet]
+        public ActionResult<InMemoryStockDb> GetStocks()
+        {
+            var temp = new List<dynamic>();
+            foreach(var x in InMemoryStockDb.dailyStockDetails)
+            {
+                x.ToString();
+                temp.Add(x);
+                
+            }
+            if (temp != null)
+                return Ok(temp);
+            else
+                return BadRequest();
+        }
+    }
+}
---
+++ Models/InMemoryStockDb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailySharePrice.Models
+{
+    public class InMemoryStockDb : IInMemoryStockDb
+    {
+        public static List<DailyStockDetails> dailyStockDetails = new List<DailyStockDetails> {
+            new DailyStockDetails { StockId = 1, StockName = "Bajaj Finance",StockValue=100},
+            new DailyStockDetails { StockId = 2, StockName = "Asian Paints", StockValue = 200 },
+            new DailyStockDetails { StockId = 3, StockName = "Adani Ports", StockValue = 300 },
+            new DailyStockDetails { StockId = 4, StockName = "Bajaj Automobiles", StockValue = 400 },
+            new DailyStockDetails { StockId = 5, StockName = "Havells India", StockValue = 500 }
+            };
+
+        public DailyStockDetails DailySharePrice(string stockName)
+        {
+            return dailyStockDetails.FirstOrDefault(c=>c.StockName.ToLower()==stockName.ToLower());
+        }
+
+        
+
+
+    }
+}
